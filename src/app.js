bc224f42bdae1b50a033d84936e419eadacd50a7
Kartik <kartikmalik019@gmail.com>
2022-07-24 14:06:09 +0530

    404 postman collection

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use("/retailer", retailerRouter)
 
 
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.status(200).send("Application is up and running - refer to the postman collection https://www.getpostman.com/collections/8a463250b9edf2faca98")
 });
 
 module.exports = app;
